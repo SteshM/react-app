cd1e19fcd3e06029554819c4b0cbd019119a6b30
SteshM <stella.mutua@imali.click>
2025-04-23 14:03:30 +0300

    made a few changes

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,7 @@
 //Pascalcasing---capitalize the first letters...just like CamelCase
 
-function Message(){
+export default function Message(){
     //JSx, javascript html
     return <h1>Hello world</h1>;
 }
 
-//to use it export 
-export default Message;
