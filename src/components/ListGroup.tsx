79ea8289f1398f9ac08cb2dc92c9baccc198f53e
SteshM <stella.mutua@imali.click>
2025-04-23 15:01:55 +0300

    Rendering lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,20 @@
-
 export default function ListGroup() {
+  //adding fragments instead of importing fragments from react, just add empty <>
+  //in jsx, we use {} to render data dynamically
+  //No for loops
+  //when rendering items in react , we need to give each a unique key for identity, especially when using 'map'
+  //incase we are consuming from an api, each item has an id, so we use ids
 
-    //adding fragments instead of importing fragments from react, just add empty <>
-    
+  const items = ["Nairobi", "Eldoret", "Kisumu", "Emali", "Nakuru"];
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key = {item}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
